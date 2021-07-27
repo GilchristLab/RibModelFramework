ff2f7be5d6f7cda219456824ad72380f30d83a57
Michael Gilchrist <mikeg@utk.edu>
2021-07-27 12:41:29 -0400

    added 'start <-2' when start < 2 in combineTwoDimensionalTrace

--- R/parameterObject.R
+++ R/parameterObject.R
@@ -1965,6 +1965,7 @@ combineTwoDimensionalTrace <- function(trace1, trace2,start=2,end=NULL){
   if(start < 2)
   {
     print("Start must be at least 2 because the last element of first trace is first of second trace. Setting start = 2.")
+    start <- 2
   }
   if(is.null(end) || end <= start)
   {
